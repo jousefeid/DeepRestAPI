d4d40160d13f26a41580b00f7e27f7b0271b1e55
Yousef Eid <yeid53646@gmail.com>
2026-06-23 20:24:27 +0300

    JWT in Suagger
    Configure Swagger JWT Authorization

--- DeepRestAPI/Extention/CustomJwtAuthExtention.cs
+++ DeepRestAPI/Extention/CustomJwtAuthExtention.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace DeepRestAPI.Extention
@@ -28,6 +29,50 @@ namespace DeepRestAPI.Extention
                 };
             });
                 
+        }
+
+        public static void AddSwaggerGenJwtAuth(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "DeepRestAPI",
+                    Version = "v1",
+                    Description = "ECommerce-DeepRestAPI",
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "Youssef",
+                        Email = "youssefeidomar@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/youssefeid-")
+                    }
+                });
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },    
+                        new List<string>()
+                    }
+                });
+            });
         }
     }
 }
